dd57027b39e8819715314a670c1ab988665d4bdc
derek dargatz <derek@pop-os.localdomain>
2020-11-21 15:51:38 -0500

    now rollin' 2 thirteen sided dice!

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,12 @@
+import random
 def main():
-  print('You rolled a die')
+    dice_rolls = 2
+    dice_sum = 0
+    for i in range(0,dice_rolls):
+        roll = random.randint(1,12)
+        dice_sum += roll
+        print(f'\n\tYou rolled a: {roll}\n')
+    print(f'\n\tYou have rolled a total of: {dice_sum}', '@' * dice_sum)
 
 if __name__== "__main__":
   main()
